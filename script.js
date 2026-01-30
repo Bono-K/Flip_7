423d23484838a6c1c676ec965acf3d3d74382998
Bono-K <71620264+Bono-K@users.noreply.github.com>
2026-01-30 13:48:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ function stopInteraction() {
 
 function handleMouseClick(e) {
   if (e.target.matches("[selectKey]")) {
-    const playerCount = e.selectKey
+    const playerCount = e.target.dataset.selectKey
 	showAlert(Number(playerCount), 5000)	
     showAlert(playerCount, 5000)	
     playerSelect[0].classList.add("inactive")
