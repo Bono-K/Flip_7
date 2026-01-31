3737a1fb40d7e1adc739b33ace00a884830ede94
Bono-K <71620264+Bono-K@users.noreply.github.com>
2026-01-31 11:44:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -260,7 +260,7 @@ function playerChange() {
 	guessGrid[4].textContent=keyboard[activePlayer*7+4].textContent	
 	guessGrid[5].textContent=keyboard[activePlayer*7+5].textContent	
 	guessGrid[6].textContent=keyboard[activePlayer*7+6].textContent	
-	for (let i=0; i<7; i++) {
+	for (let i=0; i<round; i++) {
 		let updateAmount = String(guessGrid[i].textContent)
 		//showAlert(activePlayer*7+round,5000)
 		//showAlert(i, 5000)
